1c8b333bac2c80862465b26d5c272c69a0a837fe
Petar Cevriz <pcevriz95@gmail.com>
2023-05-22 17:58:29 +0200

    Create UI input component

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import GoButton from "./ui/go-button.svelte";
+	import GoInput from "./ui/go-input.svelte";
 
 	let counter = 0;
 
@@ -7,8 +8,15 @@
 		counter += 1;
 		console.log(counter);
 	}
+
+	let username = "Test";
+	function setUsername(event: CustomEvent) {
+		username = event.detail;
+	}
 </script>
 
-<h1>Hello World!</h1>
+<h1>Hello {username}</h1>
+<!-- Input example -->
+<GoInput placeholder="Username" value={username} on:input={setUsername} />
 <!-- Button example -->
 <GoButton text={`Login ${counter}`} on:click={incrementCounter} disabled={counter === 10} />
---
+++ webapp/src/routes/ui/go-input.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	// TODO Add validation ZOD?
+	import { createEventDispatcher } from "svelte";
+
+	export let value = "";
+
+	const dispatch = createEventDispatcher();
+
+	function handleInput(event: Event) {
+		if (event.target) {
+			dispatch("input", (event.target as HTMLInputElement).value);
+		}
+	}
+</script>
+
+<input class="input" {...$$props} {value} on:input={handleInput} />
+
+<style>
+	.input {
+		background-color: #d5dcdc;
+		border: none;
+		border-radius: 5px;
+		color: #738f80;
+		font-size: 16px;
+		font-weight: bold;
+		padding: 16px 8px;
+	}
+
+	.input:focus {
+		outline: none;
+	}
+</style>
